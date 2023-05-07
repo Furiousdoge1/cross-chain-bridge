5a24f6c9275fc831ba519391cd16d55dcb1f7c08
Furiousdoge1 <132780279+Furiousdoge1@users.noreply.github.com>
2023-05-07 13:14:52 +0300

    Delete providers directory

--- providers/TransactionProviders.ts
+++
@@ -1,9 +0,0 @@
-import { Gateway } from "@renproject/ren";
-import { createStateContext } from "react-use";
-
-export type GatewayContext = Gateway | null;
-
-const [useGatewayContext, GatewayContextProvider] =
-  createStateContext<GatewayContext>(null);
-
-export { useGatewayContext, GatewayContextProvider };
--- providers/multiwallet/MultiwalletProvider.tsx
+++
@@ -1,6 +0,0 @@
-import { MultiwalletProvider as RenMultiwalletProvider } from "@renproject/multiwallet-ui";
-import React, { FunctionComponent } from "react";
-
-export const MultiwalletProvider: FunctionComponent = ({ children }) => {
-  return <RenMultiwalletProvider>{children}</RenMultiwalletProvider>;
-};
